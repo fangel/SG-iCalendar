ba4734ae61d5f02c339b8b6070409ae39d2c1369
fangel <fangel@sevengoslings.net>
2008-02-27 08:50:57 +0000

    Updated VEvent. Duration instead of DTEnd now works. Added query-methods for start and end of event

--- blocks/SG_iCal_VEvent.php
+++ blocks/SG_iCal_VEvent.php
@@ -32,22 +32,19 @@ class SG_iCal_VEvent {
 		$this->uid = $data['uid']->getData();
 		unset($data['uid']);
 		
-		$this->start = strtotime($data['dtstart']->getData());
-		unset($data['dtstart']);
-		if( isset($data['dtstart']['tzid']) ) {
-			echo 'Start has TZ' . "\n";
-			$ts = $ical->getTimeZoneInfo($data['dtstart']['tzid']);
-			$offset = $ts->getOffset($this->start);
-			$this->start = strtotime(gmdate('D, d M Y H:i:s', $this->start) . ' ' . $offset);
+		if( isset($data['dtstart']) ) {
+			$this->start = $this->getTimestamp( $data['dtstart'] );
+			unset($data['dtstart']);
 		}
-
-		$this->end = strtotime($data['dtend']->getData());
-		unset($data['dtend']);
-		if( isset($data['dtend']['tzid']) ) {
-			echo 'End has TZ' . "\n";
-			$ts = $ical->getTimeZoneInfo($data['dtend']['tzid']);
-			$offset = $ts->getOffset($this->end);
-			$this->end = strtotime(gmdate('D, d M Y H:i:s', $this->end) . ' ' . $offset);
+		
+		if( isset($data['dtend']) ) {
+			$this->end = $this->getTimestamp($data['dtend']);
+			unset($data['dtend']);
+		} elseif( isset($data['duration']) ) {
+			require_once dirname(__FILE__).'/../helpers/SG_iCal_Duration.php'; // BUILD: Remove line
+			$dur = new SG_iCal_Duration( $data['duration']->getData() );
+			$this->end = $this->start + $dur->getDuration();
+			unset($data['duration']);
 		}
 
 		$imports = array('summary','description','location');
@@ -99,5 +96,36 @@ class SG_iCal_VEvent {
 	 */
 	public function isConfirmed() {
 		return (isset($this->data['status']) && $this->data['status'] == 'CONFIRMED');
+	}
+	
+	/**
+	 * Returns the timestamp for the beginning of the event
+	 * @return int
+	 */
+	public function getStart() {
+		return $this->start;
+	}
+	
+	/**
+	 * Returns the timestamp for the end of the event
+	 * @return int
+	 */
+	public function getEnd() {
+		return $this->end;
+	}
+	
+	/**
+	 * Calculates the timestamp from a DT line.
+	 * @param $line SG_iCal_Line
+	 * @return int
+	 */
+	private function getTimestamp( SG_iCal_Line $line ) {
+		$ts = strtotime($line->getData());
+		if( isset($data['dtend']['tzid']) ) {
+			$tz = $ical->getTimeZoneInfo($line['tzid']);
+			$offset = $tz->getOffset($ts);
+			$ts = strtotime(gmdate('D, d M Y H:i:s', $ts) . ' ' . $offset);
+		}
+		return $ts;
 	}
 }

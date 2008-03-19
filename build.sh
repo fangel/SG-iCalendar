2c4dc52c76257c6865c77ff6d2dacd5f597941de
fangel <fangel@sevengoslings.net>
2008-03-19 09:42:45 +0000

    Changed build-script to first take SG_iCalReader, then the helpers then the blocks

--- build.sh
+++ build.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-find . | grep SG_iCal | grep -v svn | xargs cat | grep -v "BUILD: Remove line"
+cat SG_iCalReader.php | grep -v "BUILD: Remove line"
+find ./helpers | grep SG_iCal | grep -v svn | xargs cat | grep -v "BUILD: Remove line"
+find ./blocks | grep SG_iCal | grep -v svn | xargs cat | grep -v "BUILD: Remove line"

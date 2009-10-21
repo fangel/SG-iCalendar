097b861a4ecdec9ac298ec20b08fbc8d512e3ee2
Steven Oxley <steven.aj.oxley@gmail.com>
2009-10-21 17:56:14 -0400

    Changed SG_iCalReader.php to SG_iCal.php in build.sh to accomodate the name change.

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-cat SG_iCalReader.php | grep -v "BUILD: Remove line"
+cat SG_iCal.php | grep -v "BUILD: Remove line"
 find ./helpers | grep SG_iCal | grep -v svn | xargs cat | grep -v "BUILD: Remove line"
 find ./blocks | grep SG_iCal | grep -v svn | xargs cat | grep -v "BUILD: Remove line"
